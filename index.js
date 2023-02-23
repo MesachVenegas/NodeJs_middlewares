e4b7c36bd823fd8ab3f5f643ffae08a721245566
Mesach Venegas Santana <99062079+MesachVenegas@users.noreply.github.com>
2023-02-23 08:33:52 -0800

    change: add basic config server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express();
+
+
+// app.use()
+
+app.get('/', (req, res) =>{
+    res.send('Welcome NodeJs')
+})
+
+// activa el servidor en modo escucha para captar todo lo que sucede en el servidor.
+app.listen(8000, () =>{
+    console.log('Servidor en linea en el puerto 8000');
+})
